cf34708677b9d0e40a705b92ff28cc296446c3a0
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-27 13:49:27 +0530

    UC6 branch commit

---
+++ UC6.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class UC6
+{
+    public static final int IS_FULL_TIME=1;
+    public static final int IS_PART_TIME=2;
+    public static final int EMP_RATE_PER_HOUR=20;
+    public static final int NUM_OF_WORKING_DAYS=20;
+    public static final int MAX_WORK_HOURS=100;
+
+	public static void main(String args[])
+	{
+        int empHrs=0,empWage=0,totalWage=0;
+        int hours=0,days=1;
+
+        while(hours<=MAX_WORK_HOURS && days<=NUM_OF_WORKING_DAYS)
+        {   
+            int empCheck=((int)Math.floor(Math.random()+5))%3;
+            //System.out.println(empCheck);
+            
+            switch(empCheck)
+            {
+                case IS_FULL_TIME:  empHrs=8;
+                                    break;	
+                case IS_PART_TIME:  empHrs=4;
+                                    break;
+                        default:    empHrs=0;
+            }
+
+            empWage=empHrs*EMP_RATE_PER_HOUR;
+            System.out.println("Daily Employee Wage: "+empWage);
+            
+            totalWage=totalWage+empWage;
+
+            hours=hours+empHrs;
+            days++;
+        }
+
+        System.out.println("Total Wage: "+totalWage);
+        System.out.println("Total Working Hours: "+hours);
+        System.out.println("Total working days: "+(days-1)); /*As days get incremented after day completion hence -1, to determine total working days*/
+	}
+}
