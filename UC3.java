1ad05f58fb638169fc9c101ef0ecdde56efe3244
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-27 12:19:44 +0530

    UC3 branch commit

---
+++ UC3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class UC3
+{
+    public static final int IS_FULL_TIME=1;
+    public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+
+	public static void main(String args[])
+	{
+		int empHrs=0,empWage=0;
+
+		int empCheck=(int)Math.floor(Math.random()+15)%3;
+		System.out.println(empCheck);
+		
+		if(empCheck==IS_FULL_TIME)
+            empHrs=8;	
+        else if(empCheck==IS_PART_TIME)
+            empHrs=4;
+		else
+			empHrs=0;
+
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+
+		System.out.println("Daily Employee Wage: "+empWage);
+	}
+}
