586abd7f53bc7766c6fa04bc31ae847d74b040db
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-27 11:57:03 +0530

    UC2 branch commit

---
+++ UC2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class UC2
+{
+	public static final int IS_FULL_TIME=1;
+	public static final int EMP_RATE_PER_HOUR=20;
+
+	public static void main(String args[])
+	{
+		int empHrs=0,empWage=0;
+
+		double empCheck=Math.floor(Math.random()+15)%2;
+		//System.out.println(empCheck);
+		
+		if(empCheck==IS_FULL_TIME)
+			empHrs=8;	
+		else
+			empHrs=0;
+
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+
+		System.out.println("Daily Employee Wage: "+empWage);
+	}
+}
